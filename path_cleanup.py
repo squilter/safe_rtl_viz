1facdff36c4446142d069388fe5cdaf84e673e4e
squilter <squilter@mit.edu>
2017-06-05 16:01:20 -0400

    update comment with new TODO

--- path_cleanup.py
+++ path_cleanup.py
@@ -39,7 +39,6 @@ def segment_segment_dist(p1, p2, p3, p4):
     t1, t2 = 0,0 # the parameter for the position on line1 and line2 which define the closest points.
 
     if(D < 0.0000001): # almost parallel. This avoid division by 0.
-        # TODO what about two parallel lines which are closer than 2 meters apart, but where 1 is much longer than the other?
         p1p3 = sqrt((x3-x1)**2+(y3-y1)**2+(z3-z1)**2), ( (x3-x1)/2, (y3-y1)/2, (z3-z1)/2 )
         p1p4 = sqrt((x4-x1)**2+(y4-y1)**2+(z4-z1)**2), ( (x4-x1)/2, (y4-y1)/2, (z4-z1)/2 )
         p2p3 = sqrt((x3-x2)**2+(y3-y2)**2+(z3-z2)**2), ( (x3-x2)/2, (y3-y2)/2, (z3-z2)/2 )
@@ -82,6 +81,8 @@ def segment_segment_dist(p1, p2, p3, p4):
 
     TODO optimization ideas:
         - implement clean_pos which records where we have cleaned up to. Also implement all optimizations that go with it
+            - RDP algorithm (defined recursively) will return immediately if both points are before clean_pos
+            - pruning algorithm will not compare any 2 lines before clean_pos. The second for-loop should start at clean_pos
         - implement a cut-off that stops the path from growing any more. Max 50 points, then throw an exception if it can't be cleaned up more.
         - If our return path is too long, maybe use a more aggressive cleanup.
 
